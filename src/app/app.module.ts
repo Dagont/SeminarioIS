0e7ecb220006b9b01dd1d51b5a670d47b807ca08
Daniel Nieto <dagont-@hotmail.com>
2018-05-22 17:34:27 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
+import { PagesModule } from './pages/pages.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
+
+// Modulos
+import { PagesComponent } from './pages/pages.component';
+
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { RegisterComponent } from './login/register/register.component';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
